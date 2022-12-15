32c0adcbf16b6ac18ec6ebc9aba0032303d05c27
Irakli Gubelidze <igubelidze@ip.local>
2022-12-15 17:35:43 +0400

    fixed

--- pages/index.jsx
+++ pages/index.jsx
@@ -80,7 +80,12 @@ const Home = () => {
 
 //one page scroll by mouse wheel
 if(isMobile) {
-    document.addEventListener("touchmove", function (event) {
+  window.addEventListener('touchstart', function(event){
+    //console.log(event);
+    
+    
+  })
+    window.addEventListener("touchend", function (event) {
       event.preventDefault()
         if (inScroll === false) {
             inScroll = true;
